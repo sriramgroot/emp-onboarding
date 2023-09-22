dc85a66a95fd6c76142cd143a76048e643ad84af
Sriram Karthick G <sriram.karthick@grootan.com>
2023-09-22 23:51:04 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Steppers from './components/blocks/steppers';
+
+function App() {
+  return (
+    <div className="App">
+      <Steppers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+const ERROR_MESSAGE = {
+    InvalidFile: "File Format doesn't support"
+}
+
+const FLOW = {
+    SelectionType: "Employment Type",
+    UploadDocuments: "Upload Required Documents",
+    SignOfferLetter: "Sign Offer Letter",
+    WelcomeCourse: "Your Courses",
+    SalaryDetails: "Salary Details",
+    SignNDADocs: "NDA Documents to Sign",
+    PreviewInfo: "Preview Your Details"
+}
+
+export const Configuration = {
+    ERROR_MESSAGE: ERROR_MESSAGE,
+    FLOW: FLOW
+}
